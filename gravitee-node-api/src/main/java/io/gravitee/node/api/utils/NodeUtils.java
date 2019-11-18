b530bcfe22148328385cc3f0fa6de9fb46d7d6bb
Nicolas Géraud <nicolas.geraud@gmail.com>
2019-11-18 18:07:46 +0100

    feat(services): Add a user-agent header for all services requests
    fix gravitee-io/issues#2854

---
+++ gravitee-node-api/src/main/java/io/gravitee/node/api/utils/NodeUtils.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.node.api.utils;
+
+import io.gravitee.common.util.Version;
+import io.gravitee.node.api.Node;
+
+public class NodeUtils {
+    public static String userAgent(Node node) {
+        return node == null ? null :
+                "Gravitee.io/"
+                        + Version.RUNTIME_VERSION.MAJOR_VERSION
+                        + " ("
+                        + node.application()
+                        + "; "
+                        + node.name()
+                        + "; "
+                        + node.id()
+                        +")";
+    }
+}
