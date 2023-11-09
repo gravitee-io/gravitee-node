660662a4a80cd787538e2ce9c0e8e560e54ce582
Kamiel Ahmadpour <elitegimail@gmail.com>
2023-11-09 13:15:24 +0100

    feat: add support for Gravitee Pem Registry (#257)

--- gravitee-node-kubernetes/src/test/java/io/gravitee/node/kubernetes/keystoreloader/KubernetesConfigMapKeyStoreLoaderTest.java
+++ gravitee-node-kubernetes/src/test/java/io/gravitee/node/kubernetes/keystoreloader/KubernetesConfigMapKeyStoreLoaderTest.java
@@ -15,6 +15,9 @@
  */
 package io.gravitee.node.kubernetes.keystoreloader;
 
+import static io.gravitee.node.api.certificate.KeyStoreLoader.GRAVITEEIO_PEM_REGISTRY;
+import static io.gravitee.node.kubernetes.keystoreloader.KubernetesSecretKeyStoreLoader.*;
+import static io.gravitee.node.kubernetes.keystoreloader.KubernetesSecretKeyStoreLoader.KUBERNETES_TLS_KEY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -22,6 +25,7 @@ import io.gravitee.kubernetes.client.KubernetesClient;
 import io.gravitee.kubernetes.client.api.ResourceQuery;
 import io.gravitee.kubernetes.client.model.v1.ConfigMap;
 import io.gravitee.kubernetes.client.model.v1.ObjectMeta;
+import io.gravitee.kubernetes.client.model.v1.Secret;
 import io.gravitee.node.api.certificate.KeyStoreBundle;
 import io.gravitee.node.api.certificate.KeyStoreLoader;
 import io.gravitee.node.api.certificate.KeyStoreLoaderOptions;
@@ -31,6 +35,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.security.KeyStoreException;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
@@ -126,6 +131,78 @@ public class KubernetesConfigMapKeyStoreLoaderTest {
 
         assertNotNull(keyStoreBundle);
         assertEquals(1, keyStoreBundle.getKeyStore().size());
+    }
+
+    @Test
+    public void shouldLoadGraviteePemRegistry() throws IOException, KeyStoreException {
+        final KeyStoreLoaderOptions options = KeyStoreLoaderOptions
+            .builder()
+            .withKeyStoreType(KeyStoreLoader.CERTIFICATE_FORMAT_PEM_REGISTRY)
+            .withKubernetesLocations(Collections.singletonList("/gio/configmaps/" + KeyStoreLoader.GRAVITEEIO_PEM_REGISTRY))
+            .withKeyStorePassword("secret")
+            .withWatch(false)
+            .build();
+
+        cut = new KubernetesConfigMapKeyStoreLoader(options, kubernetesClient);
+
+        final ConfigMap pemRegistry = new ConfigMap();
+
+        final HashMap<String, String> pemRegistryData = new HashMap<>();
+        pemRegistryData.put("localhost", "my-tls-secret1");
+        pemRegistryData.put("localhost2", "my-tls-secret2");
+        pemRegistry.setData(pemRegistryData);
+
+        final ObjectMeta pemRegistryMetadata = new ObjectMeta();
+        pemRegistryMetadata.setName(KeyStoreLoader.GRAVITEEIO_PEM_REGISTRY);
+        pemRegistryMetadata.setNamespace("gio");
+        pemRegistryMetadata.setAnnotations(Collections.singletonMap(GRAVITEEIO_PEM_REGISTRY, "true"));
+        pemRegistryMetadata.setUid("/namespaces/gio/configmaps" + KeyStoreLoader.GRAVITEEIO_PEM_REGISTRY);
+        pemRegistry.setMetadata(pemRegistryMetadata);
+
+        final Secret secret1 = new Secret();
+        secret1.setType(KUBERNETES_TLS_SECRET);
+
+        final HashMap<String, String> data1 = new HashMap<>();
+        data1.put(KUBERNETES_TLS_CRT, readContent("localhost.cer"));
+        data1.put(KUBERNETES_TLS_KEY, readContent("localhost.key"));
+        secret1.setData(data1);
+
+        final ObjectMeta metadata1 = new ObjectMeta();
+        metadata1.setName("my-tls-secret1");
+        metadata1.setUid("/namespaces/gio/secrets/my-tls-secret");
+        secret1.setMetadata(metadata1);
+
+        final Secret secret2 = new Secret();
+        secret2.setType(KUBERNETES_TLS_SECRET);
+
+        final HashMap<String, String> data2 = new HashMap<>();
+        data2.put(KUBERNETES_TLS_CRT, readContent("localhost2.cer"));
+        data2.put(KUBERNETES_TLS_KEY, readContent("localhost2.key"));
+        secret2.setData(data2);
+
+        final ObjectMeta metadata2 = new ObjectMeta();
+        metadata2.setName("my-tls-secret2");
+        metadata2.setUid("/namespaces/gio/secrets/my-tls-secret2");
+        secret2.setMetadata(metadata2);
+
+        Mockito
+            .when(kubernetesClient.get(ResourceQuery.<ConfigMap>from("/gio/configmaps/" + KeyStoreLoader.GRAVITEEIO_PEM_REGISTRY).build()))
+            .thenReturn((Maybe.just(pemRegistry)));
+        Mockito
+            .when(kubernetesClient.get(ResourceQuery.<Secret>from("/gio/secrets/my-tls-secret1").build()))
+            .thenReturn(Maybe.just(secret1));
+        Mockito
+            .when(kubernetesClient.get(ResourceQuery.<Secret>from("/gio/secrets/my-tls-secret2").build()))
+            .thenReturn(Maybe.just(secret2));
+
+        AtomicReference<KeyStoreBundle> bundleRef = new AtomicReference<>(null);
+        cut.addListener(bundleRef::set);
+        cut.start();
+
+        final KeyStoreBundle keyStoreBundle = bundleRef.get();
+
+        assertNotNull(keyStoreBundle);
+        assertEquals(2, keyStoreBundle.getKeyStore().size());
     }
 
     private String readContent(String resource) throws IOException {
