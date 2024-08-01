8ff992319dc461a75f0ae819a0087529dca703ba
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2024-08-01 17:26:00 +0200

    feat: allow merge of several gravitee.yml

--- gravitee-node-container/src/main/java/io/gravitee/node/container/spring/env/PropertiesConfiguration.java
+++ gravitee-node-container/src/main/java/io/gravitee/node/container/spring/env/PropertiesConfiguration.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,14 +17,15 @@ package io.gravitee.node.container.spring.env;
 
 import io.gravitee.node.api.secrets.resolver.PropertyResolverFactoriesLoader;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
@@ -43,12 +44,27 @@ public class PropertiesConfiguration {
 
         YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
 
-        String yamlConfiguration = System.getProperty(GRAVITEE_CONFIGURATION);
-        Resource yamlResource = new FileSystemResource(yamlConfiguration);
+        String yamlConfigurations = System.getProperty(GRAVITEE_CONFIGURATION);
+        if (yamlConfigurations == null) {
+            throw new IllegalArgumentException(GRAVITEE_CONFIGURATION + " must be set");
+        }
 
-        LOGGER.info("\tGravitee configuration loaded from {}", yamlResource.getURL().getPath());
+        List<FileSystemResource> yamlResources = Stream.of(yamlConfigurations.split(",")).map(FileSystemResource::new).toList();
 
-        yaml.setResources(yamlResource);
+        boolean first = true;
+        for (FileSystemResource yamlResource : yamlResources) {
+            final String action;
+            if (first) {
+                action = "loaded";
+                first = false;
+            } else {
+                action = "overridden";
+            }
+            LOGGER.info("\tGravitee configuration {} from {}", action, yamlResource.getURL().getPath());
+        }
+
+        yaml.setResources(yamlResources.toArray(new FileSystemResource[] {}));
+
         Properties properties = yaml.getObject();
         LOGGER.info("Loading Gravitee configuration. DONE");
 
